e5bc28255410642cb230b6a914a867d1da149729
ShabihZaidi <shabih.fatima@live.com>
2018-08-26 15:13:31 +0400

    Add files via upload

---
+++ blocks.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Aug 26, 2018 at 03:10 PM
+-- Server version: 5.7.21
+-- PHP Version: 5.6.35
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `flightpath`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blocks`
+--
+
+DROP TABLE IF EXISTS `blocks`;
+CREATE TABLE IF NOT EXISTS `blocks` (
+  `bid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `section` varchar(255) NOT NULL DEFAULT '',
+  `region` varchar(255) NOT NULL DEFAULT '',
+  `module` varchar(255) NOT NULL DEFAULT '',
+  `delta` varchar(255) NOT NULL DEFAULT '',
+  `weight` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`bid`)
+) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `blocks`
+--
+
+INSERT INTO `blocks` (`bid`, `section`, `region`, `module`, `delta`, `weight`) VALUES
+(59, 'system_main', 'right_col', 'system', 'admin_tools', 1),
+(60, 'system_main', 'left_col', 'announcements', 'primary', 2),
+(61, 'system_main', 'right_col', 'blocks', 'block_1', 0),
+(62, 'system_main', 'left_col', 'blocks', 'block_2', 1),
+(66, 'system_login', 'top', 'system', 'login_form', 1),
+(67, 'system_login', 'top', 'blocks', 'block_2', 0);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
